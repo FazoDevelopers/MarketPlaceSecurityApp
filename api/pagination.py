3945391ee320892afdb67bb60e6e4dd3020a6721
javohirtech <javohirtech@gmail.com>
2023-09-28 17:20:03 +0500

    add frontend

--- api/pagination.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.pagination import PageNumberPagination
-
-
-class CameraPagination(PageNumberPagination):
-    page_size = 10
-
-
-class CriminalsPagination(PageNumberPagination):
-    page_size = 10
