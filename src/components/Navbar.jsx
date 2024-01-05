b940f6bb730c32b0d93502f213b0c822217ebad3
javohirtech <javohirtech@gmail.com>
2024-01-06 00:12:56 +0500

    added key button without generate with API

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,29 @@
-import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { handleError, handleSuccess } from "../utils/globals";
-import { api } from "../services/api";
+import { handleError } from "../utils/globals";
 
 export default function Navbar() {
-  const keyGenerate = async() => {
-    try{
-      const response = await api.get("")
-    }catch(err){
-      handleError("Generatsiyada xatolik!")
+  const [kalit, setKalit] = useState("");
+
+  const keyGenerate = async () => {
+    try {
+      // Simulating an asynchronous operation
+      // Replace this with your actual asynchronous logic
+      // const response = await api.get("");
+      // console.log(response);
+      setKalit("kalit so'z");
+    } catch (err) {
+      handleError("Generatsiyada xatolik!");
     }
   };
+
+  const handleKeyGenerateClick = () => {
+    keyGenerate();
+  };
+
   return (
     <div className="sticky top-0 z-50 bg-black navbar_wrapper">
-      <div className="container flex flex-wrap max-w-screen-lg p-4 mx-auto justify-evenly">
+      <div className="container flex flex-wrap items-center max-w-screen-lg p-4 mx-auto justify-evenly">
         <Link
           to="/home"
           className="flex items-center w-full px-3 py-2 mb-4 font-extrabold text-white border-8 border-gray-400 md:mb-0 md:w-auto"
@@ -44,11 +54,14 @@ export default function Navbar() {
         </Link>
         <button
           type="button"
-          className="px-5 text-black bg-white"
-          onChange={() => keyGenerate()}
+          className="px-5 text-white"
+          title="Kalit generatsiya qilish"
+          onClick={handleKeyGenerateClick}
         >
-          <i className="fas fa-key"></i>
+          <i className="fas fa-key fa-2x"></i>
         </button>
+
+        {kalit ? <p className="text-white">{kalit}</p> : null}
       </div>
     </div>
   );
